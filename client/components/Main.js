8b172b53e68618cd4928e75990e7826fea26bdcb
Joshua Whateley <JoshuaW@healthlink.net>
2017-11-27 09:28:03 +1300

    added the redux store and the action creator files

--- client/components/Main.js
+++ client/components/Main.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import {Link } from 'react-router';
 
 export class Main extends React.Component{
-
     render(){
         return(
             <div>
                 <h1>
                     <Link to='/'>Reduxtagram</Link>
                 </h1>
+                {React.cloneElement(this.props.children, this.props)}
             </div>
         )
     }
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 import {render} from 'react-dom';
-import {Main} from './components/Main'
+import {Main} from './components/Main';
+import {PhotoGrid} from './components/PhotoGrid';
+import {Single} from './components/Single';
+
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 import css from './styles/style.styl';
 
-render(<Main/>, 
-document.getElementById('root'));
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component= {Main}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path="/view/:postId" component={Single}></Route>
+        </Route>
+    </Router>
+);
+
+render(router, document.getElementById('root'));
 
 
